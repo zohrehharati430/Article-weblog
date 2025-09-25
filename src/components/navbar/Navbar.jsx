fe0e07dd0822bf0ff4167a53932924b778d46e86
zohreh <zohreh.harati@gmail.com>
2025-09-25 13:53:30 -0800

    Refactor App component to include Home; update Navbar to accept title prop

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,9 @@
 import styled from './navbar.module.css'
-function Navbar(){
-    
+function Navbar(props){
+
   return(
     <div  className={styled.header}>
+        <h3>{props.title}</h3>
       <ul>
         <li>لیست مقالات</li>
         <li>مقاله جدید</li>
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../../components/navbar/Navbar"
+
+function Home(props){
+    return(
+       <div>
+        
+        <Navbar title="بلاگ"/>
+       </div> 
+    )
+}
+export default Home
